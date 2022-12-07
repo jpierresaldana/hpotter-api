a61f2171d2e108892719c7ad18e772ee82fc7a27
Jean Pierre <jp.saldana@outlook.es>
2022-12-07 18:04:42 -0500

    controllers ok, routes ok, seed ok

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,37 +1,33 @@
 class CharactersController < ApplicationController
-  before_action :set_character, only: %w[show edit update destroy]
+  before_action :set_character, only: %w[show update destroy]
 
   def index
-    @characters = Character.all
+    characters = Character.all
   end
 
-  def show
-  end
+  def show; end
 
   def create
-    @character = Character.new(characters_params)
-    if @character.save
-      flash[:notice] = "Personaje creada con éxito"
+    character = Character.new(characters_params)
+    if character.save
+
     else
-      render :new, status: :unprocessable_entity
-      flash[:notice] = "ERROR - Revise los datos a registrar para crear el Personaje"
+
     end
   end
 
   def update
-    @character.update!(characters_params)
-    flash[:notice] = "Personaje actualizado con éxito"
+    character.update!(characters_params)
   end
 
   def destroy
-    @character.destroy
-    flash[:notice] = "Personaje eliminado con éxito"
+    character.destroy
   end
 
   private
 
   def set_character
-    @character = Character.find(params[:id])
+    character = Character.find(params[:id])
   end
 
   def characters_params
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,65 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "----------------------------------------------------------------------------------------------------------"
+puts "Eliminando DB"
+Character.destroy_all if Rails.env.development?
+puts "----------------------------------------------------------------------------------------------------------"
+
+puts "Creando Characters"
+
+puts "Character 1"
+character = Character.new(
+  actor: "Rupert Grint",
+  name: "Ron Weasly",
+  review: "El sexto de los siete hermanos Weasley. Empieza en Hogwarts a la vez que Harry.
+  Coinciden en el tren y se hacen inseparables.",
+  image_url: "https://res.cloudinary.com/abdulaba/image/upload/v1669900365/Harry/Ron-Weasley__lyqtwc.jpg"
+)
+character.save
+puts "Created character 1"
+
+puts "Character 2"
+character2 = Character.new(
+  actor: "Daniel Radcliffe",
+  name: "Harry Potter",
+  review: "El elegido. A los 11 descubre que es mago, que estudiará en Hogwarts
+  y que Lord Voldemort asesinó a sus padres cuando él era un bebé.",
+  image_url: "https://res.cloudinary.com/abdulaba/image/upload/v1669900365/Harry/Harry-Potter__htwqje.jpg"
+)
+character2.save
+puts "Created character 2"
+
+puts "Character 3"
+character3 = Character.new(
+  actor: "Emma Watson",
+  name: "Hermione Granger",
+  review: "La eterna amiga de Harry y Ron es toda una empollona.
+  De no ser por sus conocimientos, las aventuras de Harry habrían sido diferentes.",
+  image_url: "https://res.cloudinary.com/abdulaba/image/upload/v1669900364/Harry/Hermione-Granger__ze0mnx.jpg"
+)
+character3.save
+puts "Created character 3"
+
+puts "Character 4"
+character4 = Character.new(
+  actor: "Richard Harris",
+  name: "Albus Dumbledore",
+  review: "Director de Hogwarts. El mago más poderoso que ha existido. A medida que Harry crezca,
+  le irá pasando las claves para vencer a Voldemort.",
+  image_url: "https://res.cloudinary.com/abdulaba/image/upload/v1669900364/Harry/Albus-Dumbledore__ztskyf.jpg"
+)
+character4.save
+puts "Created character 4"
+
+puts "Character 5"
+character5 = Character.new(
+  actor: "Brendan Gleeson",
+  name: "'Ojoloco' Moody",
+  review: "Ex auror contratado por Dumbledore para cubrir la siempre vacante
+  plaza de Profesor Contra las Artes Oscuras. Es muy desconfiado.",
+  image_url: "https://res.cloudinary.com/abdulaba/image/upload/v1669900365/Harry/Alastor-Ojoloco-Moody__seldth.jpg"
+)
+character5.save
+puts "Created character 5"
+
+puts "----------------------------------------------------------------------------------------------------------"
+puts "Completed"
+puts "----------------------------------------------------------------------------------------------------------"
